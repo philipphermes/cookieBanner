165bcea511799aaba552c6c83e986d31cbfafe45
Philipp Anton Hermes <philipphermes2001@gmail.com>
2022-10-07 08:47:43 +0200

    allow to change libk and cookieLifeTime more easyly

--- cookieBanner.js
+++ cookieBanner.js
@@ -1,52 +1,62 @@
 const cookieBanner = document.getElementById('cookieBanner');
+const link = '#cookiesInfo';
+const cookieBannerLifeTime = 365;
 
-//Texts for languages
-const textEn = "When you visit any website, it may store or retrieve information on your browser, mostly in the form of cookies. " +
-    "This information might be about you, your preferences or your device and is mostly used to make the site work as you expect it to. " +
-    "The information does not usually directly identify you, but it can give you a more personalized web experience. " +
-    "Because we respect your right to privacy, you can choose not to allow some types of cookies. However, " +
-    "blocking some types of cookies may impact your experience of the site and the services we are able to offer. " +
-    "<a href='#cookies'>Learn more</a>";
-const textDe = "Wenn Sie eine Website besuchen, kann diese Informationen in Ihrem Browser speichern oder abrufen, " +
-    "meistens in Form von Cookies. Diese Informationen können sich auf Sie, Ihre Präferenzen oder Ihr Gerät beziehen und " +
-    "werden hauptsächlich verwendet, damit die Website so funktioniert, wie Sie es erwarten. Die Informationen " +
-    "identifizieren Sie normalerweise nicht direkt, können Ihnen jedoch ein personalisierteres Web-Erlebnis bieten. " +
-    "Da wir Ihr Recht auf Privatsphäre respektieren, können Sie einige Arten von Cookies ablehnen. Das Blockieren " +
-    "einiger Arten von Cookies kann jedoch Ihre Erfahrung mit der Website und den Diensten, " +
-    "die wir anbieten können, beeinträchtigen. <a href='#cookies'>Erfahre mehr</a>"
-const htmlEn = "<div class='bottom'>" +
-    "<div class='text'>" + textEn + "</div>" +
-    "<div class='checks'>" +
-    "<div class='checkBox'><input type='checkbox' id='performanceCheckBox'><label for='performanceCheckBox'>Performance</label></div>" +
-    "<div class='checkBox'><input type='checkbox' id='functionalCheckBox'><label for='functionalCheckBox'>Functional</label></div>" +
-    "<div class='checkBox'><input type='checkbox' id='marketingCheckBox'><label for='marketingCheckBox'>Marketing</label></div>" +
-    "<button onclick='blockBanner()' id='blockAll'>Block All</button>" +
-    "<button onclick='acceptAll()' id='acceptAll'>Accept All</button>" +
-    "<button onclick='saveBanner()' id='saveBanner'>Save</button>" +
-    "</div></div>";
+generateBanner();
+removeElements();
 
-const htmlDe = "<div class='bottom'>" +
-    "<div class='text'>" + textDe + "</div>" +
-    "<div class='checks'>" +
-    "<div class='checkBox'><input type='checkbox' id='performanceCheckBox'><label for='performanceCheckBox'>Performance</label></div>" +
-    "<div class='checkBox'><input type='checkbox' id='functionalCheckBox'><label for='functionalCheckBox'>Funktionale</label></div>" +
-    "<div class='checkBox'><input type='checkbox' id='marketingCheckBox'><label for='marketingCheckBox'>Marketing</label></div>" +
-    "<button onclick='blockBanner()' id='blockAll'>Alle Blockieren</button>" +
-    "<button onclick='acceptAll()' id='acceptAll'>Alle Akzeptieren</button>" +
-    "<button onclick='saveBanner()' id='saveBanner'>Speichern</button>" +
-    "</div></div>";
+function generateBanner() {
+    let html = '';
 
-let html = '';
+    if (document.documentElement.lang === 'de') {
+        html = getDeHtml();
+    } else {
+        html = getEnHtml();
+    }
 
-if (document.documentElement.lang === 'de') {
-    html = htmlDe;
-} else {
-    html = htmlEn;
+    cookieBanner.innerHTML = html;
 }
 
-cookieBanner.innerHTML = html;
+function getDeHtml() {
+    const textDe = "Wenn Sie eine Website besuchen, kann diese Informationen in Ihrem Browser speichern oder abrufen, " +
+        "meistens in Form von Cookies. Diese Informationen können sich auf Sie, Ihre Präferenzen oder Ihr Gerät beziehen und " +
+        "werden hauptsächlich verwendet, damit die Website so funktioniert, wie Sie es erwarten. Die Informationen " +
+        "identifizieren Sie normalerweise nicht direkt, können Ihnen jedoch ein personalisierteres Web-Erlebnis bieten. " +
+        "Da wir Ihr Recht auf Privatsphäre respektieren, können Sie einige Arten von Cookies ablehnen. Das Blockieren " +
+        "einiger Arten von Cookies kann jedoch Ihre Erfahrung mit der Website und den Diensten, " +
+        "die wir anbieten können, beeinträchtigen. <a href='" + link + "'>Erfahre mehr</a>"
 
-removeElements();
+    return "<div class='bottom'>" +
+        "<div class='text'>" + textDe + "</div>" +
+        "<div class='checks'>" +
+        "<div class='checkBox'><input type='checkbox' id='performanceCheckBox'><label for='performanceCheckBox'>Performance</label></div>" +
+        "<div class='checkBox'><input type='checkbox' id='functionalCheckBox'><label for='functionalCheckBox'>Funktionale</label></div>" +
+        "<div class='checkBox'><input type='checkbox' id='marketingCheckBox'><label for='marketingCheckBox'>Marketing</label></div>" +
+        "<button onclick='blockBanner()' id='blockAll'>Alle Blockieren</button>" +
+        "<button onclick='acceptAll()' id='acceptAll'>Alle Akzeptieren</button>" +
+        "<button onclick='saveBanner()' id='saveBanner'>Speichern</button>" +
+        "</div></div>";
+}
+
+function getEnHtml() {
+    const textEn = "When you visit any website, it may store or retrieve information on your browser, mostly in the form of cookies. " +
+        "This information might be about you, your preferences or your device and is mostly used to make the site work as you expect it to. " +
+        "The information does not usually directly identify you, but it can give you a more personalized web experience. " +
+        "Because we respect your right to privacy, you can choose not to allow some types of cookies. However, " +
+        "blocking some types of cookies may impact your experience of the site and the services we are able to offer. " +
+        "<a href='" + link + "'>Learn more</a>";
+
+    return "<div class='bottom'>" +
+        "<div class='text'>" + textEn + "</div>" +
+        "<div class='checks'>" +
+        "<div class='checkBox'><input type='checkbox' id='performanceCheckBox'><label for='performanceCheckBox'>Performance</label></div>" +
+        "<div class='checkBox'><input type='checkbox' id='functionalCheckBox'><label for='functionalCheckBox'>Functional</label></div>" +
+        "<div class='checkBox'><input type='checkbox' id='marketingCheckBox'><label for='marketingCheckBox'>Marketing</label></div>" +
+        "<button onclick='blockBanner()' id='blockAll'>Block All</button>" +
+        "<button onclick='acceptAll()' id='acceptAll'>Accept All</button>" +
+        "<button onclick='saveBanner()' id='saveBanner'>Save</button>" +
+        "</div></div>";
+}
 
 function removeElements() {
     const cookie = getCookie('cookieBanner');
@@ -68,7 +78,7 @@ function removeElements() {
             performanceCheckBox.checked = false;
 
             for(let i = 0; i < performanceElements.length; i++) {
-                performanceElements[i].innerHTML = "<p>Blocked by Cookie Banner</p>";
+                performanceElements[i].innerHTML = "";
             }
         } else {
             performanceCheckBox.checked = true;
@@ -78,7 +88,7 @@ function removeElements() {
             functionalCheckbox.checked = false;
 
             for(let i = 0; i < functionalElements.length; i++) {
-                functionalElements[i].innerHTML = "<p>Blocked by Cookie Banner</p>";
+                functionalElements[i].innerHTML = "";
             }
         } else {
             functionalCheckbox.checked = true;
@@ -88,7 +98,7 @@ function removeElements() {
             marketingCheckbox.checked = false;
 
             for(let i = 0; i < marketingElements.length; i++) {
-                marketingElements[i].innerHTML = "<p>Blocked by Cookie Banner</p>";
+                marketingElements[i].innerHTML = "";
             }
         } else {
             marketingCheckbox.checked = true;
@@ -104,7 +114,7 @@ function blockBanner() {
     const data = {'performance': 'block', 'functional': 'block', 'marketing': 'block'};
     const jsonData = JSON.stringify(data);
 
-    setCookie('cookieBanner', jsonData, 365)
+    setCookie('cookieBanner', jsonData)
 
     window.top.location.reload();
 }
@@ -136,7 +146,7 @@ function saveBanner() {
 
     const jsonData = JSON.stringify(data);
 
-    setCookie('cookieBanner', jsonData, 365)
+    setCookie('cookieBanner', jsonData)
 
     window.top.location.reload();
 }
@@ -145,7 +155,7 @@ function acceptAll() {
     const data = {'performance': 'show', 'functional': 'show', 'marketing': 'show'};
     const jsonData = JSON.stringify(data);
 
-    setCookie('cookieBanner', jsonData, 365)
+    setCookie('cookieBanner', jsonData)
 
     window.top.location.reload();
 }
@@ -165,9 +175,9 @@ function getCookie(cname) {
     return "";
 }
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue) {
     const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (cookieBannerLifeTime * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
 }
